23b229b60be778459a895d3d09ce40a40c16d388
toxicafunk <toxicafunk@gmail.com>
2016-06-06 00:18:52 +0200

    Works!

--- src/main/scala/com/mindcurv/func/XmlReader.scala
+++ src/main/scala/com/mindcurv/func/XmlReader.scala
@@ -1,35 +1,12 @@
 package com.mindcurv.func
 
-import java.net.{Authenticator, URL}
+import java.net.URL
 import javax.xml.parsers.SAXParser
 
 import org.xml.sax.InputSource
 
-import scala.annotation.tailrec
-import scala.collection.immutable.Stream.Empty
-import scala.xml.{Node, NodeSeq}
 import scala.xml.parsing.NoBindingFactoryAdapter
-
-/**
-  * Created by erodriguez on 11/05/16.
-  */
-
-/*import scales.utils._
-import ScalesUtils._
-import scales.xml._
-import ScalesXml._
-
-
-object XmlReader {
-  // Contains the document
-  val doc = loadXml(new FileReader("document.xml"))
-
-  // gets Path from the documents root
-  val path = top(doc)
-
-  // query for all nodes that match the XPath
-  // path.\*("NoNamespace").\*(prefixedQName)
-}*/
+import scala.xml.{Node, NodeSeq}
 
 
 class HTML5Parser extends NoBindingFactoryAdapter {
@@ -55,10 +32,24 @@ object XmlReader {
 
   val parser: HTML5Parser = new HTML5Parser()
 
-  def crawl(url: String, depth: Int): Seq[String] = {
+  def main(args: Array[String]): Unit = {
+    /* Authenticator.setDefault(new ProxyAuthenticator());
+
+    System.getProperties().put("http.proxyHost", "proxycorp.geci");
+    System.getProperties().put("http.proxyPort", "8080");
+    System.getProperties().put("http.proxyUser", "T0000104");
+    System.getProperties().put("http.proxyPassword", "12345678");
+    System.getProperties().put("proxySet", "true"); */
+
+    val links: Set[String] = crawl("https://toxicafunk.wordpress.com/", 0)
+    print(s"Final Result: $links")
+  }
+
+  def crawl(url: String, depth: Int): Set[String] = {
 
     def readURL(url: String): Node =
       try {
+        println(s"loading url $url")
         parser.load(new URL(url))
       } catch {
         case _: Throwable => {
@@ -73,49 +64,37 @@ object XmlReader {
     }
 
     //@tailrec
-    def loop(urls: Seq[String], acc: Seq[String], depth: Int): Seq[String] = {
+    def loop(urls: Seq[String], acc: Set[String], depth: Int): Set[String] = {
       println("depth: " + depth)
-      println(acc)
+
       def ensureAbsolute(x: String): String = {
-        if (x.startsWith("http")) x else url + x
+        if (x.startsWith("http")) x
+        else if (!x.startsWith("/")) {
+          val i: Int  = x.indexOf("/")
+          if (i != -1) {
+            val s: String = x.substring(i)
+            if (!s.isEmpty) url + s
+            else ""
+          }
+          else ""
+        }
+        else url + x
       }
 
       if (depth == 3) { println("max depth reached"); acc }
       else {
-        val l: List[String] = List[String]()
         val ls = for {
           x: String <- urls
-          page: Node <- readURL(ensureAbsolute(x))
-          link <- extractLinks(page)
-        } yield link :: l
-        val s: Seq[String] = ls.flatten
-        loop(s, s ++ acc, depth + 1)
+          page: Node <- {
+            if (!acc.contains(x)) readURL(ensureAbsolute(x))
+            else <html/>
+          }
+          links <- extractLinks(page)
+        } yield links
+        loop(ls, ls.toSet ++ acc, depth + 1)
       }
     }
 
-    loop(extractLinks(readURL(url)), Seq[String](), 0)
-  }
-
-  def main(args: Array[String]): Unit = {
-    /* Authenticator.setDefault(new ProxyAuthenticator());
-
-    System.getProperties().put("http.proxyHost", "proxycorp.geci");
-    System.getProperties().put("http.proxyPort", "8080");
-    System.getProperties().put("http.proxyUser", "T0000104");
-    System.getProperties().put("http.proxyPassword", "12345678");
-    System.getProperties().put("proxySet", "true"); */
-
-    val links: Seq[String] = crawl("http://www.scala-lang.org", 0)
-    print(links)
+    loop(extractLinks(readURL(url)).toSeq, Set[String](), 0)
   }
 }
-
-/* def readURL(url: String): List[String] = {
-      val root = parser.load(new URL(url))
-      val nodes: NodeSeq = root \\ "a"
-      nodes.foldLeft(List[String]())((acc, node) => {
-        val href: String = node.text
-        if (href.startsWith("http://")) href :: acc
-        else url + href :: acc
-      })
-    } */
